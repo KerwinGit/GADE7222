8042990e1bf87f061327410b20e34b8eff70684d
Coolguybrad <bnwicks03@gmail.com>
2023-10-23 10:44:12 +0200

    Spectators added

---
+++ Assets/TEMP FOLDER/SpectateAnimations.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectateAnimations : MonoBehaviour
+{
+
+    [SerializeField] private Animator anim;
+    [SerializeField] private float animTime;
+    private int randAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("PlayRandAnimation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayRandAnimation()
+    {
+
+        animTime = Random.Range(3,16);
+        randAnim = Random.Range(0,5);
+
+        if (randAnim == 0)
+        {
+            animTime = 1;
+        }
+
+        anim.SetInteger("randAnim", randAnim);
+
+        yield return new WaitForSeconds(animTime);
+
+        StartCoroutine(PlayRandAnimation());
+    }
+}
