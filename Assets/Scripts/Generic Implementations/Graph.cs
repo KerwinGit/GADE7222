1cfc017f115d2e8a0ceaa4454f36af9f4ebcdddd
Kerwin Naidoo <kerwinjn18@gmail.com>
2023-11-20 14:01:20 +0200

    Generic Implementation of Graph added

--- Assets/Scripts/Generic Implementations/Graph.cs
+++ Assets/Scripts/Generic Implementations/Graph.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Graph : MonoBehaviour
+public abstract class Graph<T> //generic implementation of graph
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Dictionary<T, List<T>> adjacencyList;
+
+    public Graph()
     {
-        
+        adjacencyList = new Dictionary<T, List<T>>(); //vertex: T; list of adjacent members: List<T>
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddVertex(T vertex)
     {
-        
+        if (!adjacencyList.ContainsKey(vertex)) //checks vertex doesn't already exist
+        {
+            adjacencyList[vertex] = new List<T>(); //initialises empty list of neighbours to vertex in dictionary
+        }
+    }
+
+    public abstract void AddEdge(T source, T destination);
+}
+
+public class UndirectedGraph<T> : Graph<T> // an undirected graph is one with two-way connections between each node/vertex
+{
+    public override void AddEdge(T source, T destination)
+    {
+        AddVertex(source);
+        AddVertex(destination);
+
+        adjacencyList[source].Add(destination); //adds the destination vertex to the adjacency list of the source vertex
+        adjacencyList[destination].Add(source); //adds the source vertex to the adjacency list of the destination vertex
+    }
+}
+
+public class DirectedGraph<T> : Graph<T> // a directed graph has one-way connections
+{
+    public override void AddEdge(T source, T destination)
+    {
+        AddVertex(source);
+        AddVertex(destination);
+
+        adjacencyList[source].Add(destination); //only the destination vertex is added to the source's list as the flow goes in one direction only
     }
 }
